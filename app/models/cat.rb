26a55ae0c2161bfd30aa4bde62796c799f201c46
Andee Fonder <andeefonder@gmail.com>
2020-03-27 10:45:01 -0700

    redirect attempt

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,6 @@
 class Cat < ApplicationRecord
-    validates :name, presence: true
+    validates :name, :age, presence: true
+    validates :name, length: {minimum: 2, maximum: 10 }
+    validates :enjoys, length: { minimum: 10 }
+    validates :age, format: { with: /\A\d+\z/, message: "Please Enter a Number" }
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -40,4 +40,21 @@ describe "Cats API", type: :request do
     # Assure that the created cat has the correct attributes
     expect(new_cat.name).to eq('Buster')
   end
+
+  it "doesn't create a cat without a name" do
+    cat_params = {
+      cat: {
+        age: 2,
+        enjoys: 'Walks in the park'
+      }
+    }
+    # Send the request to the  server
+    post '/cats', params: cat_params
+    # expect an error if the cat_params does not have a name
+    expect(response.status).to eq 422
+    # Convert the JSON response into a Ruby Hash
+    json = JSON.parse(response.body)
+    # Errors are returned as an array because there could be more than one, if there are more than one validation failures on an attribute.
+    expect(json['name']).to include "can't be blank"
+  end
 end
